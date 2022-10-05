3fa912c640d4f00a8b1b4001854ed29bca054588
Talles Fagundes <makoto.git@gmail.com>
2022-10-05 19:48:40 -0300

    Analise Diagnostica exerc 02

--- seguranca_da_tecnologia_da_informacao/semana_01/analise_diagnostica_exerc_02.py
+++ seguranca_da_tecnologia_da_informacao/semana_01/analise_diagnostica_exerc_02.py
@@ -1,26 +1,71 @@
 #!/usr/bin/env python3.9
 
+
+
 # Crie duas estrutura de dictonario
+from uuid import uuid4
+
+
 materials = {
-    '0001': 'caderno',
-    '0010': 'borracha',
-    '0100': 'tabuada'
+    uuid4() : 'caderno',
+    uuid4() : 'borracha',
+    uuid4() : 'tabuada'
 }
 
-cart: dict[ str, str ] = {}
+class Cart:
+    def __init__( self ) -> None:
+        self.cart: dict[ str, str ] = {}
+        self.size: int = 0
+        pass
+
+    def add_to_cart( self, item: str ):
+        if  item == '':
+            print( 'Não é permitido adicionar um itém sem nome.')
+        elif not self.max_size():
+            self.cart[uuid4()] = item
+            self.size += 1
+            print( f'Adicionado {item} ao seu carrinho com sucesso' )
+
+    def remove_from_cart( self, key: str ):
+        if self.empty():
+            del self.cart[key]
+            self.size -= 1
+        
+
+    def max_size( self ) -> bool:
+        if self.size >= 5:
+            print( 'Carrinho cheio. Limite máximo de 5 items ocupados.' )
+            return True
+        return False
+    
+    def empty( self ) -> bool:
+        if self.size <= 0:
+            print( 'Não há o que remover nessa lista' )
+            return True
+        return False
 
 # Imprimeir a lista usando Enumarate
 def print_dict( dict: dict[str, str] ):
     id: int = 0
     for key, item in dict.items():
-        print( f'[ {id + 1} ]: Chave: {key}', item )
+        print( f'[ {id + 1} ]: Chave: {key} - Material: ', item )
         id += 1
-    print('')
+    print( '' )
 
-def add_to_cart( item ):
-    pass
 
-while True:
+
+user_cart = Cart()
+while True: 
+    user_cart.add_to_cart( 'apontador' )
+    user_cart.add_to_cart( 'apontador' )
+    user_cart.add_to_cart( 'apontador' )
+    user_cart.add_to_cart( 'aponta'    )
+    user_cart.add_to_cart( 'apontador' )
+    user_cart.add_to_cart( 'apontador' )
+    user_cart.add_to_cart( '' )
+
+
     print_dict(materials)
+    print_dict( user_cart.cart )
 
-    pass
+    break
