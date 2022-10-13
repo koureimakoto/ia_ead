c3edb4dbbff79742f0d75c632ecf221f44d53d35
Talles Fagundes <makoto.git@gmail.com>
2022-10-13 17:29:35 -0300

    Modified - Analise Diagnostica exerc 03 - Added comments in student.py

--- seguranca_da_tecnologia_da_informacao/semana_01/analise_diagnostica_exerc_03/student.py
+++ seguranca_da_tecnologia_da_informacao/semana_01/analise_diagnostica_exerc_03/student.py
@@ -1,10 +1,21 @@
-from re import A
-from typing import overload
-
-
+"""
+Student
+    __init__
+    name
+    email
+    last_name
+    __(s|g)name
+    __(s|g)email
+    __(s|g)last_name
+"""
 class Student:
-    def __init__(self, name: str = '', last_name: str = '', email: str = '') -> None:
-        self.data = [ 
+    def __init__(
+        self,
+        name      : str = '',
+        last_name : str = '',
+        email     : str = ''
+        ) -> None:
+        self.data: list[str] = [ 
             name,
             last_name,
             email
@@ -14,65 +25,72 @@ class Student:
 
     """
     Testando a utilização de chamadas via argumentos
-    Quero testar ainda usando __call__
     """
     # __NAME__
+    # Get ou Set the student name inside class
     def name(self, *args, **kwargs):
         size_args  : int = len(args)
         size_kwargs: int = len(kwargs)
         if size_args == 0 and size_kwargs == 0:
-            return self.gname()
+            return self.__gname()
         else:
-            return self.sname(*args, **kwargs)
+            return self.__sname(*args, **kwargs)
 
-
-    def sname(self, name: str):
+    # Private Set name
+    def __sname(self, name: str):
         if name == '':
             return False
         self.name_size = len(name)
         self.data[0] = name
         return True
 
-    def gname(self):
+    # Private Get Name
+    def __gname(self):
         return self.data[0]
 
-
     # __LAST_NAME__
+    # Get ou Set the student last name inside class
     def last_name(self, *args, **kwargs):
         size_args  : int = len(args)
         size_kwargs: int = len(kwargs)
         if size_args == 0 and size_kwargs == 0:
-            return self.glast_name()
+            return self.__glast_name()
         else:
-            return self.slast_name(*args, **kwargs)
+            return self.__slast_name(*args, **kwargs)
 
-    def slast_name(self, last_name: str):
+    # Private Set last name
+    def __slast_name(self, last_name: str):
         if last_name == '':
             return False
         self.last_name_size = len(last_name)
         self.data[1] = last_name
         return True
     
-    def glast_name(self):
+    # Private Get last name
+    def __glast_name(self):
         return self.data[1]
 
     # __EMAIL__
+    # Get ou Set the student e-mail inside class
     def email(self, *args, **kwargs):
         size_args  : int = len(args)
         size_kwargs: int = len(kwargs)
         if size_args == 0 and size_kwargs == 0:
-            return self.gemail()
+            return self.__gemail()
         else:
-            return self.semail(*args, **kwargs)
+            return self.__semail(*args, **kwargs)
     
-    def semail(self, email: str):
+    # Private set e-mail
+    def __semail(self, email: str):
         if email == '':
             return False
         self.data[2] = email
         return True
     
-    def gemail(self):
+    # Private get e-mail
+    def __gemail(self):
         return self.data[2]
 
+    # Full name size
     def size(self):
         return self.name_size + self.last_name_size
