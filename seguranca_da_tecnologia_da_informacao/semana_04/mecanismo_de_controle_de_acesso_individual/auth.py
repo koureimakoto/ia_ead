edf9f8a75721911f15172736362c92663f6e3e11
Talles Fagundes <makoto.git@gmail.com>
2022-10-23 17:30:50 -0300

    Atv Somativa 1 - Adicionei comentários mais explicativos

--- seguranca_da_tecnologia_da_informacao/semana_04/mecanismo_de_controle_de_acesso_individual/auth.py
+++ seguranca_da_tecnologia_da_informacao/semana_04/mecanismo_de_controle_de_acesso_individual/auth.py
@@ -3,7 +3,19 @@ from   typing              import Any
 from   requests.exceptions import HTTPError
 
 
+# ----------   class User    ----------
 class User(HTTPError):
+    """
+    Classe para armazenar registros de um novo usuário, alterações
+    ou guardar dados para manipulção
+
+    Construtor dela é vazio
+
+    user = User()
+
+    """
+
+    # ----------    __init__     ----------
     def __init__(self) -> None:
         super().__init__()
 
@@ -12,41 +24,47 @@ class User(HTTPError):
         self.__registered: bool  = False
         self.errno       : HTTPError = HTTPError()
 
-
-    """
-    Wrapper das funções de criação cria um novo usuário com verificação
-
-    create_new_user:
-        >> e-mail: String   # Qualquer tipo de e-mail que atendas formato de um
-        >> passwd: String   # O password digitado pelo usuário
-        << Boolean          # Se tudo correr bem, retorna Verdadeiro
-    """
+    # ---------- create_new_user ----------
     def create_new_user(self, email: str, passwd: str ) -> bool:
+        """
+        Wrapper das funções de criação cria um novo usuário com verificação
+
+        create_new_user:
+            >> e-mail: String   # Qualquer tipo de e-mail que atendas formato de um\n
+            >> passwd: String   # O password digitado pelo usuário\n
+            << Boolean          # Se tudo correr bem, retorna Verdadeiro\n
+        """
   
         # Verifica antecipadamente se o e-mail esta formatado corretamentes antes
         # da requisição para o server do Firebase.
-        if self.check_email(email) :
+        if self.check_email(email) and self.check_passwd(passwd):
 
             # Tenta criar um novo usuário
             try:
                 firebase = pyrebase.initialize_app(firebase_config.firebaseConfig)
                 # Registra os dados requisitados
-                self.__set_responde(
+                self.__set_response(
                     firebase.auth().create_user_with_email_and_password(email, passwd)
                 )
                 return True
             except HTTPError as e:
                 self.errno = e
                 print(e)
+                return False
+        print('Verify is your e-mail our password are correct!')
         return False
 
-
-
-    """
-    Verifica e retornar um usuário já cadastrado e um wrapper da mesma
-    função nativa no Firebase.
-    """
+    # ----------  get_sign_user  ----------
     def get_sign_user(self, email: str, passwd: str) -> bool:
+        """
+        Verifica e retornar um usuário já cadastrado e um wrapper da mesma
+        função nativa no Firebase.
+    
+        create_new_user:
+            >> e-mail: String   # Qualquer tipo de e-mail que atendas formato de um\n
+            >> passwd: String   # O password digitado pelo usuário\n
+            << Boolean          # Se tudo correr bem, retorna Verdadeiro
+        """
 
         if self.check_email(email) and self.check_passwd(passwd):
 
@@ -55,84 +73,89 @@ class User(HTTPError):
                 firebase = pyrebase.initialize_app(firebase_config.firebaseConfig)
 
                 # Registra os dados requisitados
-                self.__set_responde(
+                self.__set_response(
                     firebase.auth().sign_in_with_email_and_password(email, passwd)
                 )
                 return True
             except HTTPError as e:
                 self.errno = e
                 print(e)
+                return False
+        print('Verify is your e-mail our password are correct!')
         return False
 
-
-
-    """
-    Verifica o e-mail atraves de expressão regular antes de um resquest para o Firebase
-
-    check_email:
-        >> email: String   # Qualquer tipo de e-mail que atendas formato de um
-        << Boolean
-    """
+    # ----------   check_email   ----------
     def check_email(self, email: str):
+        """
+        Verifica o e-mail atraves de expressão regular antes de um resquest para o Firebase
+
+        check_email:
+            >> email: String   # Qualquer tipo de e-mail que atendas formato de um\n
+            << Boolean
+        """
         return re.findall(r'[\w\.-]+@[\w\.-]+\.[\w]+', email) and len(email) < 256
 
-
-
-    """
-    Verifica se o password possuí alphanérico case caixa alta, baixa e simbolos
-    entre 8 a 20 caracteres
-
-    check_email:
-        >> passwd: String   # Toda senha que segue a regra acima
-        << Boolean
-    """
+    # ----------  check_passwd   ----------
     def check_passwd(self, passwd: str) -> bool:
-        return re.findall(r'^(?=.*?[aA0-9Zz])(?=.*?[#?!@$ %^&*-]).{8,}', passwd) == passwd
+        """
+        Verifica se o password possuí alphanérico case caixa alta, baixa e simbolos
+        entre 8 a 20 caracteres
 
+        check_email:
+            >> passwd: String   # Toda senha que segue a regra acima\n
+            << Boolean
+        """
+        # 
+        # ^  -> Inicio de Linha
+        # (?=.*?[aA0-9Zz])    -> Busque 0 ou qalquer valor entre [a-z][A-Z][0-9]
+        # (?!.*?[\ \n\r\t])   -> Ignore qualquer espaço branco 
+        # (?=.*?[#?!@$%^&*-]) -> Busque 0 ou qalquer valor entre [ dentro dos colchetes ]
+        # .{8, 100}$          -> minimo 8 máximo 100
+        # $  -> Encerre no final da linha 
+        #
+
+        return re.findall(r'^(?=.*?[aA0-9Zz])(?!.*?[\ \n\r\t])(?=.*?[#?!@$%^&*-]).{8,100}$', passwd) == passwd
     
-    
-    """
-    Torna o e-mail registrado da classe 
-    """
+    # ----------    get_email    ----------
     def get_email(self) -> str:
+        """
+        Torna o e-mail registrado da classe 
+        """
         return self.__email
 
-
-
-    """
-    Mantem reservado o token de autenticação ao invés da senha.
-    """
+    # ----------   __id_token    ----------
     def __id_token(self) -> str:
+        """
+        Mantem reservado o token de autenticação ao invés da senha.
+        """
         return self.__token
 
+    # ---------- __set_response  ----------
+    def __set_response(self, auth) -> None:
+        """
+        Prepara todos os dados importantes para serem armazenados na class
 
-
-    """
-    Prepara todos os dados importantes para serem armazenados na class
-
-    __set_response[private]
-        >> auth: firebase.Auth   # Os dados do resquest ao Firebase
-        << None
-    """
-    def __set_responde(self, auth) -> None:
-        print(auth)
+        __set_response[private]
+            >> auth: firebase.Auth   # Os dados do resquest ao Firebase\n
+            << None
+        """
         self.__email     = auth['email'     ]
         self.__token     = auth['idToken'   ]
         self.__registered= True
 
-
-
-    """
-    Retorna se tem algum registro armazenado em formato de texto
-
-    is_registered:
-        << String(Yes|No)
-    """
+    # ----------  is_registerd   ----------
     def is_registered(self) -> str :
+        """
+        Retorna se tem algum registro armazenado em formato de texto
+
+        is_registered:
+            << String(Yes|No)
+        """
         if self.__registered == True:
             return 'Yes'
         return 'No'
 
+    # ----------     __str__     ----------
     def __str__(self) -> str:
         return 'User e-mail: ' + self.get_email() + '\n' + self.is_registered()
         
--- seguranca_da_tecnologia_da_informacao/semana_04/mecanismo_de_controle_de_acesso_individual/main.py
+++ seguranca_da_tecnologia_da_informacao/semana_04/mecanismo_de_controle_de_acesso_individual/main.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
+from auth import User
 
-import re
-import auth
+"""
+Como Usar:
 
-new_user = auth.User()
+    Primeiro importar o arquivo 'auth' para acessar a classe 'User'
+    Você pode acessar de duas formas
 
+        '
+        import auth
+        
+        user = auth.User(NONE)
+        
+        '-> Essa primeira parte importa tudo que existe em Auth
+
+        -- OU --
+
+        '
+        from auth import User
+
+        user = User(NONE)
+        
+        '-> Dessa forma importará apenas a classe User
+
+
+        A classe User possuí algums métodos Publicos, que são 
+        funções dentro  da classe que você pode executar fora
+        dela. E quase todos seus dados são ReadyOnly, que não
+        permite  sobrescrever uma vez  escritos. Sendo  todos 
+        dados privados Privados, isto é, para acessar estes a
+        qualquer momento é  necessário invocar metódos que os 
+        retornaram, claro  só os que  não comprometam a inte-
+        gridade dos dados da classe.
+
+        para 
+
+"""
+
+
+
+
+new_user = User()
 new_user.create_new_user("urei@gmail.com", "#$Talles1234")
-print(new_user.check_passwd("Oii$AOPALS"))
